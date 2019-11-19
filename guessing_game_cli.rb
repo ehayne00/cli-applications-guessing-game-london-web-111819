8237fd7db408581ceebfb41cd691677bd0b855e0
Emily Haynes <ehayne00@hotmail.com>
2019-11-19 08:55:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,12 +1,13 @@
 def run_guessing_game
   random_number = rand(6) + 1
+  prompt_user = "Guess a number between 1 and 6."
   input = gets
   input.chomp
   if input == random_number
-    return "You guessed the correct number!"
+    puts "You guessed the correct number!"
     elsif input == "exit"
-    return "Goodbye!"
+    puts "Goodbye!"
   else 
-    return "Sorry! The computer guessed #{random_number}."
+    "Sorry! The computer guessed #{random_number}."
   end
 end
